4ed1687c6fa64d611ec00b4379ee81ed3d57e0d4
kosten00 <ylkahu@gmail.com>
2020-05-13 12:45:04 +0700

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,57 @@
+<script>
+import Modal from './Modal.svelte'
+
+let people = [
+	{
+		name: 'Yoshi', beltColor: 'black', age: 25, id: 1
+	},{
+		name: 'Mario', beltColor: 'orange', age: 45, id: 2
+	},{
+		name: 'Luigi', beltColor: 'brown', age: 35, id: 3
+	}
+];
+
+const handleClick = (id) => {
+	people = people.filter((person) => person.id !== id);
+}
+
+let num = 5;
+</script>
+
+<Modal />
+<main>
+	{#each people as person (person.id)}
+		<div>
+		<h4>{person.name}</h4>
+		{#if person.beltColor === 'black'}
+			<p><strong>Master ninja</strong></p>
+		{/if}
+		<p>{person.age} years old, {person.beltColor} belt</p>
+		<button on:click={(e) => {handleClick(e, person.id)}}>delete</button>
+		</div>
+	{:else}
+		<p>There no people to show</p>	
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Modal.svelte
@@ -0,0 +1,13 @@
+<script>
+    let showModal = false;
+</script>
+
+{#if showModal}
+<div class="backdrop">
+    <div class="modal">
+    <p>Modal window</p>
+    </div>
+</div>
+{/if}
+
+<style></style>
